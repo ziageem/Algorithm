e327bb34b9f12f5c7f5d682cab144b0aad9f5e6a
JIAH KIM <79643680+ziageem@users.noreply.github.com>
2021-03-24 22:13:23 +0900

    Create 2389.py

---
+++ 2389.py
@@ -0,0 +1,15 @@
+#BAEKJOON 2389 설탕배달
+
+n=int(input())
+count=0
+
+while True:
+    if n%5==0:
+        count+=n//5
+        print(count)
+        break
+    n-=3
+    count+=1
+    if n<0:
+      print(-1)
+      break
